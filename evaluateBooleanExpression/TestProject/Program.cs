29a3a6f701b9d05299fb0009ff56f8f790402006
Jhonrell <jhonrellg@gmail.com>
2026-02-11 03:43:19 +0800

    Login Access Level

---
+++ evaluateBooleanExpression/TestProject/Program.cs
@@ -0,0 +1,79 @@
+﻿string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact and Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient priveleges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient priveleges.");
+}
+
+/* Random coin = new Random();
+Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails"); */
+
+
+/* Random coin = new Random();
+int flip = coin.Next(0, 2);
+Console.WriteLine((flip == 0) ? "heads" : "tails"); */
+
+/* int saleAmount = 1001;
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}"); */
+
+/* int saleAmount = 1001;
+int discount = saleAmount > 1000  ? 100 : 50;
+Console.WriteLine($"Discount: {discount}"); */
+
+/* int a = 7;
+int b = 6;
+Console.WriteLine(a != b);
+string s1 = "Hello";
+string s2 = "Hello";
+Console.WriteLine(s1 != s2); */
+
+/* string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow")); */
+
+
+/* Console.WriteLine(pangram.Contains("fox") == false);
+Console.WriteLine(!pangram.Contains("fox")); */
+
+/* Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow")); */
+
+/* Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1); */
+
+/* Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+
+string myValue = "a";
+Console.WriteLine(myValue != "a");
+ 
+string value1 = " a";
+string value2 = "A ";
+
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower()); */
